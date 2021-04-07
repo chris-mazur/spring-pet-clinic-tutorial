6b6232e849dd01e9598b4b212cc278f8dc85cf46
Chris <kpm42@bath.ac.uk>
2021-04-08 00:06:07 +0100

    Spring Initializr

---
+++ src/test/java/com/github/chrismazur/learningpetclinic/SpringPetClinicTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.chrismazur.learningpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
